75719da55724d4a9c9df0a21bc6a0ac951b45b7c
tiedsandi <fachransandi@gmail.com>
2025-03-29 14:11:05 +0700

    add dashboard and profile pages; update detail page title; remove unused detail_user and users pages

---
+++ pages/profile.php
@@ -0,0 +1,26 @@
+<?php session_start();?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Profile Page</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</head>
+<body class="bg-gray-100">
+  <main class="max-w-7xl mx-auto p-4 md:p-8">
+    <?php include '../inc/layouts/header.php'; ?>
+
+
+
+    <?php include '../inc/layouts/footer.php'; ?> 
+  </main>
+
+
+
+  <script src="../assets/js/script.js"></script>
+</body>
+</html>
